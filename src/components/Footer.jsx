6be1461fd6c3426c0912cd203f3d5ed598496f54
Geraskin Aleksey <119218654+geraskinalekseysergeevich@users.noreply.github.com>
2024-01-17 17:07:23 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import classes from './UI/Footer.module.css';
 
 const Footer = ( {footerInitialsText} ) => {
 
     const emailAddress = 'leha.geraskin.80@gmail.com'
 
     return (
-        <footer className='footer__container'>
-            <div className="footer-body__container">
-                <div className="footer-initials">{footerInitialsText}</div>
-                <div className="footer-year">2023</div>
-                <a href={`mailto:${emailAddress}`} className='footer-email'>
+        <footer className={classes.footer__container}>
+            <div className={classes.footer-body__container}>
+                <div>{footerInitialsText}</div>
+                <div>2023</div>
+                <a href={`mailto:${emailAddress}`}>
                     {emailAddress}
                 </a>
             </div>
