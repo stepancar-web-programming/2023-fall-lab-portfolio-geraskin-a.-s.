d1a9a5ae1e4b50b8b45fd8038e2e6531db4ecf37
Geraskin Aleksey <119218654+geraskinalekseysergeevich@users.noreply.github.com>
2024-01-19 18:55:12 +0300

    Update SwitchButton.jsx

--- src/components/SwitchButton.jsx
+++ src/components/SwitchButton.jsx
@@ -3,7 +3,7 @@ import classes from './UI/SwitchButton.module.css';
 
 const SwitchButton = ({changeLangFunc, lang}) => {
     return (
-        <label className={classes.language-btn classes.switch}>
+        <label className={`${classes.language-btn} ${classes.switch}`}>
             <input type="checkbox"/>
             <div className={classes.slider} onClick={() => changeLangFunc(lang)}></div>
             <div className={classes.language-title ${lang === 'ru' ? classes.activated : ''}}>{lang}</div>
