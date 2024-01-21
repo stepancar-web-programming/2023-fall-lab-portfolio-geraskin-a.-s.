3f5cfbdd7243d2e2fb629ca1d367ddb7af105923
Geraskin Aleksey <119218654+geraskinalekseysergeevich@users.noreply.github.com>
2024-01-21 03:48:52 +0300

    Update DotWrapper.jsx

--- src/components/DotWrapper.jsx
+++ src/components/DotWrapper.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Dot from './Dot'; // Путь к вашему компоненту Dot
 import classes from './UI/DotWrapper.module.css';
+import cn from 'classnames';
 
 const DotWrapper = ({slideIndex, func, currentSlide}) => {
+
+    const isActive = slideIndex === currentSlide
+    
     return (
         <div 
-            className=`${slideIndex === currentSlide ? classes.dot-active : ''}`
+            className={cn({ [classes['active']]: isActive })}
             onClick={() => func(slideIndex > currentSlide ? slideIndex - currentSlide : slideIndex - currentSlide)}
         >
             <Dot/>
